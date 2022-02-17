3f10bd423cbf7220f2ade5cdf7e652730ae2b0da
Mustafa Kamel <mostafak252@gmail.com>
2022-02-17 22:12:41 +0200

    Add search template

--- search/views.py
+++ search/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 
 @require_http_methods(["GET"])
 def search(request):
-    pass
+    return render(request, 'index.html')
 
 
 @require_http_methods(["POST"])
