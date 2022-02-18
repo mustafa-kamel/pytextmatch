8d1475d318f276e1a8a70969bb8e51c7aa441449
Mustafa Kamel <mostafak252@gmail.com>
2022-02-18 16:54:20 +0200

    Add views tests

---
+++ search/tests/test_views.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestUrls(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_search_accepts_only_get(self):
+        response = self.client.get(reverse('search:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_search_rejects_post(self):
+        response = self.client.post(reverse('search:index'))
+        self.assertEqual(response.status_code, 405)
+        self.assertTemplateNotUsed(response, 'index.html')
+        self.assertTemplateNotUsed(response, 'results.html')
+
+    def test_results_accepts_only_post_empty_body(self):
+        response = self.client.post(reverse('search:results'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTrue('message' in response.context)
+
+    def test_results_accepts_only_post_has_body(self):
+        response = self.client.post(
+            reverse('search:results'), {'query': 'finde me'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'results.html')
+        self.assertTrue('result' in response.context)
+
+    def test_results_rejects_get(self):
+        response = self.client.get(reverse('search:results'))
+        self.assertEqual(response.status_code, 405)
+        self.assertTemplateNotUsed(response, 'index.html')
+        self.assertTemplateNotUsed(response, 'results.html')
