474f4d9a98aaaeae7457d7cf150d2b84fc47a4a3
Mustafa Kamel <mostafak252@gmail.com>
2022-02-18 21:50:24 +0200

    Use dropdown instead of an input field

--- search/views.py
+++ search/views.py
@@ -1,27 +1,19 @@
+import os
+import pandas
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 from ratelimit.decorators import ratelimit
-
-
-def get_result(query):
-    values = [{"value": "result 1", "percentage": "%91"},
-              {"value": "result 2", "percentage": "%85"},
-              {"value": "result 3", "percentage": "%66"},
-              {"value": "result 4", "percentage": "%59"},
-              {"value": "result 5", "percentage": "%51"},
-              ]
-    return {
-        "key": query,
-        "values": values,
-        "length": len(values) + 1
-    }
+from .search import Search
 
 
 @ratelimit(key='ip', rate='5/m')
 @require_http_methods(["GET"])
 def search(request):
-    return render(request, 'index.html')
+    with open(
+            os.path.join(os.path.dirname(__file__), 'data.csv'), "r") as f:
+        options = pandas.read_csv(f, usecols=['Key'])
+    return render(request, 'index.html', {'options': options.Key.to_list()})
 
 
 @ratelimit(key='ip', rate='5/m')
@@ -32,4 +24,4 @@ def results(request):
                       {'message': 'Please enter a valid search query.'})
 
     return render(request, 'results.html',
-                  {"result": get_result(request.POST.get('query'))})
+                  {"result": Search(request.POST.get('query')).get_result()})
