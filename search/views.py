36a771d277e602eec9910e88ae1e3cbaaf0740bd
Mustafa Kamel <mostafak252@gmail.com>
2022-02-18 15:24:06 +0200

    Add results view logic

--- search/views.py
+++ search/views.py
@@ -4,6 +4,20 @@ from django.views.decorators.http import require_http_methods
 from ratelimit.decorators import ratelimit
 
 
+def get_result(query):
+    values = [{"value": "result 1", "percentage": "%91"},
+              {"value": "result 2", "percentage": "%85"},
+              {"value": "result 3", "percentage": "%66"},
+              {"value": "result 4", "percentage": "%59"},
+              {"value": "result 5", "percentage": "%51"},
+              ]
+    return {
+        "key": query,
+        "values": values,
+        "length": len(values) + 1
+    }
+
+
 @ratelimit(key='ip', rate='5/m')
 @require_http_methods(["GET"])
 def search(request):
@@ -13,4 +27,9 @@ def search(request):
 @ratelimit(key='ip', rate='5/m')
 @require_http_methods(["POST"])
 def results(request):
-    pass
+    if not request.POST.get('query'):
+        return render(request, 'index.html',
+                      {'message': 'Please enter a valid search query.'})
+
+    return render(request, 'results.html',
+                  {"result": get_result(request.POST.get('query'))})
