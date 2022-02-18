00bb8fc20f23fd0b9ba3a2849b644cf41e8129dd
Mustafa Kamel <mostafak252@gmail.com>
2022-02-18 02:06:00 +0200

    Use rate limit on all views

--- search/views.py
+++ search/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from ratelimit.decorators import ratelimit
 
+
+@ratelimit(key='ip', rate='5/m')
 @require_http_methods(["GET"])
 def search(request):
     return render(request, 'index.html')
 
 
+@ratelimit(key='ip', rate='5/m')
 @require_http_methods(["POST"])
 def results(request):
     pass
